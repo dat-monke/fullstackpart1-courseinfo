1cf91cd3b0f3c0422c322e9b56712943f53b8ec0
David Taing <dtaing1@uci.edu>
2023-02-24 10:57:39 -0800

    footer created and fragment utilized

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-const App = () => {
+const Footer = () => {
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='George' />
-      <Hello name='Daisy' />
+      App created by <a href='https://github.com/dat-monke'>David</a>
     </div>
+  )
+}
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
   )
 }
 
